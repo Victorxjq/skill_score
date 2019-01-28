f401b7e03e1014d8bf2c7ab6837e676704a647a7
奚骏泉 <junquan.xi@ifchange.com>
2019-01-28 16:08:07 +0800

    update run.py

--- skill_score/read_file_join.py
+++ skill_score/read_file_join.py
@@ -24,10 +24,11 @@ def extract_cv_info(line):
     info=json.loads(line[1])
     res={id:{'cv_tag':info['cv_tag']}}
     return res
+    print(res)
 
 
 #load data
 if __name__ == '__main__':
     sc = SparkContext(appName='join_cv')
     test_path='/basic_data/icdc/algorithms/20190115/icdc_20/data__fcca1aa1_41ca_4df3_bf56_feea812b5d5d'
-    sc.textFile(test_path).flatMap(extract_cv_info).collect()
+    sc.textFile(test_path).flatMap(extract_cv_info)
