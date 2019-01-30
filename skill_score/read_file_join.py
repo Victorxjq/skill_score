3e3f1a464c4857158fc1f36997121e32230efe20
奚骏泉 <junquan.xi@ifchange.com>
2019-01-30 16:34:49 +0800

    update run.py

--- skill_score/read_file_join.py
+++ skill_score/read_file_join.py
@@ -87,10 +87,10 @@ if __name__ == '__main__':
         if subprocess.call(cmd,shell=True)==1:
             if len(file_path)>0:
                 if index == 0:
-                    inp_all_algorithm = sc.textFile(file_path).Map(extract_cv_info_algorithm)
+                    inp_all_algorithm = sc.textFile(file_path).map(extract_cv_info_algorithm)
                     index += 1
                 else:
-                    tmp = sc.textFile(file_path).Map(extract_cv_info_algorithm)
+                    tmp = sc.textFile(file_path).map(extract_cv_info_algorithm)
                     inp_all_algorithm = inp_all_algorithm.union(tmp)
                     index += 1
     print('start load basic files')
@@ -101,10 +101,10 @@ if __name__ == '__main__':
         if subprocess.call(cmd, shell=True) == 1:
             if len(file_path) > 0:
                 if index == 0:
-                    inp_all_basic = sc.textFile(file_path).Map(extract_cv_info_basic)
+                    inp_all_basic = sc.textFile(file_path).map(extract_cv_info_basic)
                     index += 1
                 else:
-                    tmp = sc.textFile(file_path).Map(extract_cv_info_basic)
+                    tmp = sc.textFile(file_path).map(extract_cv_info_basic)
                     inp_all_basic = inp_all_basic.union(tmp)
                     index += 1
     # inp_all_algorithm=inp_all_algorithm.filter((lambda x:x==''))
