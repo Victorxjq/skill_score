bde815ae5a90ccbcbb486a4539b8e7ee92d52737
奚骏泉 <junquan.xi@ifchange.com>
2019-02-13 16:14:49 +0800

    update run.py

--- skill_score/read_file_join.py
+++ skill_score/read_file_join.py
@@ -99,27 +99,29 @@ if __name__ == '__main__':
         basic_file_path = '/basic_data/icdc/resumes_extras/20190115/icdc_%s' % str(val)
         print('start load algorithm files')
         index = 0
-        # for file_path in get_files_list(algorithm_file_path):
-        for file_path in ['/basic_data/icdc/algorithms/20190115/icdc_0/data__ff0f1b40_5207_4f3c_83d0_8f03b7185372']:
-            cmd = 'hadoop fs -test -d %s' % file_path
+        for alg_file_path in get_files_list(algorithm_file_path):
+        # for file_path in ['/basic_data/icdc/algorithms/20190115/icdc_0/data__ff0f1b40_5207_4f3c_83d0_8f03b7185372']:
+            cmd = 'hadoop fs -test -d %s' % alg_file_path
             if subprocess.call(cmd, shell=True) == 1:
-                if len(file_path) > 0:
+                if len(alg_file_path) > 0:
                     if index == 0:
-                        inp_all = sc.textFile(file_path).map(extract_cv_info_algorithm)
+                        inp_all = sc.textFile(alg_file_path).map(extract_cv_info_algorithm)
                         index += 1
                     else:
-                        tmp = sc.textFile(file_path).map(extract_cv_info_algorithm)
+                        tmp = sc.textFile(alg_file_path).map(extract_cv_info_algorithm)
                         inp_all = inp_all.union(tmp)
                         index += 1
         print('start load basic files')
-        # for file_path in get_files_list(basic_file_path):
-        for file_path in ['/basic_data/icdc/resumes_extras/20190115/icdc_0/data__ffd132e3_a01d_4ed3_bad0_98f9b8b069c4']:
-            cmd = 'hadoop fs -test -d %s' % file_path
+        for bas_file_path in get_files_list(basic_file_path):
+        # for file_path in ['/basic_data/icdc/resumes_extras/20190115/icdc_0/data__ffd132e3_a01d_4ed3_bad0_98f9b8b069c4']:
+            cmd = 'hadoop fs -test -d %s' % bas_file_path
             if subprocess.call(cmd, shell=True) == 1:
-                if len(file_path) > 0:
-                    tmp = sc.textFile(file_path).map(extract_cv_info_basic)
+                if len(bas_file_path) > 0:
+                    tmp = sc.textFile(bas_file_path).map(extract_cv_info_basic)
                     inp_all = inp_all.union(tmp)
         print('Group_by_keys:')
+        for inp in inp_all.take(10):
+            print(inp)
         result = inp_all.reduceByKey(add)
         # result=inp_all.groupByKey().mapValues(list)
         print('save to txt:')
