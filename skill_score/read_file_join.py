64220e3ca6a28da6afe722d56dc6fea8a0a5894a
奚骏泉 <junquan.xi@ifchange.com>
2019-02-13 17:48:25 +0800

    update run.py

--- skill_score/read_file_join.py
+++ skill_score/read_file_join.py
@@ -97,7 +97,6 @@ def add(a, b):
 
 # load data
 if __name__ == '__main__':
-    # for val in ['test']:
     for val in range(0, 1):
         sc = SparkContext(appName='join_cv')
         algorithm_file_path = '/basic_data/icdc/algorithms/20190115/icdc_%s' % str(val)
@@ -105,8 +104,8 @@ if __name__ == '__main__':
         basic_file_path = '/basic_data/icdc/resumes_extras/20190115/icdc_%s' % str(val)
         print('start load algorithm files')
         index = 0
-        # for alg_file_path in get_files_list(algorithm_file_path):
-        for alg_file_path in ['/basic_data/icdc/algorithms/20190115/icdc_0/data__ff0f1b40_5207_4f3c_83d0_8f03b7185372']:
+        for alg_file_path in get_files_list(algorithm_file_path):
+        # for alg_file_path in ['/basic_data/icdc/algorithms/20190115/icdc_0/data__ff0f1b40_5207_4f3c_83d0_8f03b7185372']:
             cmd = 'hadoop fs -test -d %s' % alg_file_path
             if subprocess.call(cmd, shell=True) == 1:
                 if len(alg_file_path) > 0:
@@ -118,8 +117,8 @@ if __name__ == '__main__':
                         inp_all = inp_all.union(tmp)
                         index += 1
         print('start load basic files')
-        # for bas_file_path in get_files_list(basic_file_path):
-        for bas_file_path in ['/basic_data/icdc/resumes_extras/20190115/icdc_0/data__ffd132e3_a01d_4ed3_bad0_98f9b8b069c4']:
+        for bas_file_path in get_files_list(basic_file_path):
+        # for bas_file_path in ['/basic_data/icdc/resumes_extras/20190115/icdc_0/data__ffd132e3_a01d_4ed3_bad0_98f9b8b069c4']:
             cmd = 'hadoop fs -test -d %s' % bas_file_path
             if subprocess.call(cmd, shell=True) == 1:
                 if len(bas_file_path) > 0:
