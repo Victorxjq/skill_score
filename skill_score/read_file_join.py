3051e3278bae07344b01fbb9ea97a362caa3c84b
奚骏泉 <junquan.xi@ifchange.com>
2019-02-13 14:06:04 +0800

    update run.py

--- skill_score/read_file_join.py
+++ skill_score/read_file_join.py
@@ -120,7 +120,7 @@ if __name__ == '__main__':
         # cmd = 'hadoop fs -test -d %s' % output_path
         # if subprocess.call(cmd, shell=True) == 1:
         #     subprocess.call('hadoop fs -rm -r %s' % output_path)
-        result.take(10).foreach(print)
+        print(result.take(10))
         # result.saveAsTextFile(output_path)
         print('batch %s,completed'%str(val))
         sc.stop()
