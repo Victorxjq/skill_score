ff56bb37306aa55a607660760ef7fca84968cb4b
奚骏泉 <junquan.xi@ifchange.com>
2019-02-14 15:42:18 +0800

    update run.py

--- skill_score/read_file_join.py
+++ skill_score/read_file_join.py
@@ -97,43 +97,27 @@ def add(a, b):
 
 # load data
 if __name__ == '__main__':
-    for val in range(0, 1):
-        sc = SparkContext(appName='join_cv_all')
-        algorithm_file_path = '/basic_data/icdc/algorithms/20190115/icdc_%s' % str(val)
-        basic_file_path = '/basic_data/icdc/resumes_extras/20190115/icdc_%s' % str(val)
-        print('start load algorithm files')
-        index = 0
-        for alg_file_path in get_files_list(algorithm_file_path):
-        # for alg_file_path in ['/basic_data/icdc/algorithms/20190115/icdc_0/data__ff0f1b40_5207_4f3c_83d0_8f03b7185372']:
-            cmd = 'hadoop fs -test -d %s' % alg_file_path
-            if subprocess.call(cmd, shell=True) == 1:
-                if len(alg_file_path) > 0:
-                    if index == 0:
-                        inp_all = sc.textFile(alg_file_path).map(extract_cv_info_algorithm).filter(lambda x:x!='null' or x!=[])
-                        index += 1
-                    else:
-                        tmp = sc.textFile(alg_file_path).map(extract_cv_info_algorithm).filter(lambda x:x!='null' or x!=[])
-                        inp_all = inp_all.union(tmp)
-                        index += 1
-        print('start load basic files')
-        for bas_file_path in get_files_list(basic_file_path):
-        # for bas_file_path in ['/basic_data/icdc/resumes_extras/20190115/icdc_0/data__ffd132e3_a01d_4ed3_bad0_98f9b8b069c4']:
-            cmd = 'hadoop fs -test -d %s' % bas_file_path
-            if subprocess.call(cmd, shell=True) == 1:
-                if len(bas_file_path) > 0:
-                    tmp = sc.textFile(bas_file_path).map(extract_cv_info_basic).filter(lambda x:x!='null' or x!=[])
-                    inp_all = inp_all.union(tmp)
-        print('Group_by_keys:')
-        result = inp_all.sortByKey(ascending=True).reduceByKey(add)
-        # result=inp_all.groupByKey().mapValues(list)
-        print('save to txt:')
-        # output_path='/user/kdd_xijunquan/cv_skill_score/test'
-        output_path = '/user/kdd_xijunquan/cv_skill_score/icdc_%s' % str(val)
-        # cmd = 'hadoop fs -test -d %s' % output_path
-        # if subprocess.call(cmd, shell=True) == 1:
-        #     subprocess.call('hadoop fs -rm -r %s' % output_path)
-        for res_val in result.take(100):
-            print(res_val)
-        # result.saveAsTextFile(output_path)
-        print('batch %s,completed' % str(val))
-        sc.stop()
+    sc = SparkContext(appName='join_cv_all')
+    # algorithm_file_path = '/basic_data/icdc/algorithms/20190115/icdc_%s' % str(val)
+    algorithm_file_path = '/basic_data/icdc/algorithms/20190115/*'
+    # basic_file_path = '/basic_data/icdc/resumes_extras/20190115/icdc_%s' % str(val)
+    basic_file_path = '/basic_data/icdc/resumes_extras/20190115/*'
+    print('start load algorithm files')
+    inp_algo=sc.textFile(algorithm_file_path).map(extract_cv_info_algorithm).filter(lambda x: x != 'null' or x != [])
+    print('start load basic files')
+    inp_basic = sc.textFile(basic_file_path).map(extract_cv_info_basic).filter(lambda x:x!='null' or x!=[])
+    print('Group_by_keys:')
+    result=inp_algo.join(inp_basic)
+    # result = inp_all.sortByKey(ascending=True).reduceByKey(add)
+    # result=inp_all.groupByKey().mapValues(list)
+    print('save to txt:')
+    # output_path='/user/kdd_xijunquan/cv_skill_score/test'
+    output_path = '/user/kdd_xijunquan/cv_skill_score/icdc'
+    # cmd = 'hadoop fs -test -d %s' % output_path
+    # if subprocess.call(cmd, shell=True) == 1:
+    #     subprocess.call('hadoop fs -rm -r %s' % output_path)
+    for res_val in result.take(100):
+        print(res_val)
+    # result.saveAsTextFile(output_path)
+    print('batch,completed')
+    sc.stop()
