2aad0f01689beb7de76858e36f6161458ef381a3
奚骏泉 <junquan.xi@ifchange.com>
2019-02-18 17:19:00 +0800

    update run.py

--- skill_score/src/extract_jd_skill_lvl.py
+++ skill_score/src/extract_jd_skill_lvl.py
@@ -195,11 +195,11 @@ def get_match_sentence(extract_cv_skill):
     if extract_cv_skill:
         skill_lvl_pair = extract_cv_skill.get("skill_lvl_pair")
         if extract_cv_skill.get("cv_tag"):
-            print('keys:',extract_cv_skill["cv_tag"].keys())
+            work_id=list(extract_cv_skill["cv_tag"].keys())
             if extract_cv_skill.get("cv_tag").get(extract_cv_skill["cv_tag"].keys()[0]):
                 function_name ='test'
-                print(extract_cv_skill.get("cv_tag").get(extract_cv_skill["cv_tag"].keys()[0]).get("should")[0].split(':')[0])
-                function_id = extract_cv_skill.get("cv_tag").get(extract_cv_skill["cv_tag"].keys()[0]).get("should")[0].split(':')[0]
+                print(extract_cv_skill.get("cv_tag").get(work_id[0]).get("should")[0].split(':')[0])
+                function_id = extract_cv_skill.get("cv_tag").get(work_id[0]).get("should")[0].split(':')[0]
                 if function_name:
                     function_name = decode_escape(function_name)
                     result = [[json.dumps({function_id: function_name}, ensure_ascii=False),
