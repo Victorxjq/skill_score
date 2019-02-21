a779f4baa2ea48953925d6a4224c7dc01e8d04c6
奚骏泉 <junquan.xi@ifchange.com>
2019-02-21 10:34:27 +0800

    update run.py

--- skill_score/read_file_join.py
+++ skill_score/read_file_join.py
@@ -85,7 +85,6 @@ def extract_cv_info_basic(line):
                 work_content=info['work']
             if 'skill' in info.keys() and info['skill'] != '':
                 skill_content = info['skill']
-                # print(info['cv_tag'])
                 res = (k_id, {'work': work_content,'skill':skill_content})
             else:
                 res = 'null'
@@ -93,6 +92,7 @@ def extract_cv_info_basic(line):
             res = 'null'
     else:
         res = 'null'
+    print(res)
     return res
 
 
@@ -103,14 +103,17 @@ def add(a, b):
 # load data
 if __name__ == '__main__':
     sc = SparkContext(appName='join_cv_all')
-    algorithm_file_path = '/basic_data/icdc/algorithms/20190115/*'
-    basic_file_path = '/basic_data/icdc/resumes_extras/20190115/*'
-    print('start load algorithm files')
-    inp_algo = sc.textFile(algorithm_file_path).map(extract_cv_info_algorithm).filter(lambda x: x != 'null' or x != [])
-    print('start load basic files')
-    inp_basic = sc.textFile(basic_file_path).map(extract_cv_info_basic).filter(lambda x: x != 'null' or x != [])
-    print('Group_by_keys:')
-    output_path = '/user/kdd_xijunquan/cv_skill_score/icdc_with_skill'
-    result = inp_basic.join(inp_algo).saveAsTextFile(output_path)
-    print('batch,completed')
+    basic_file_path_test = '/basic_data/icdc/resumes_extras/20190115/icdc_0/data__ffd132e3_a01d_4ed3_bad0_98f9b8b069c4'
+    inp_basic = sc.textFile(basic_file_path_test).map(extract_cv_info_basic).filter(lambda x: x != 'null' or x != [])
+
+    # algorithm_file_path = '/basic_data/icdc/algorithms/20190115/*'
+    # basic_file_path = '/basic_data/icdc/resumes_extras/20190115/*'
+    # print('start load algorithm files')
+    # inp_algo = sc.textFile(algorithm_file_path).map(extract_cv_info_algorithm).filter(lambda x: x != 'null' or x != [])
+    # print('start load basic files')
+    # inp_basic = sc.textFile(basic_file_path).map(extract_cv_info_basic).filter(lambda x: x != 'null' or x != [])
+    # print('Group_by_keys:')
+    # output_path = '/user/kdd_xijunquan/cv_skill_score/icdc_with_skill'
+    # result = inp_basic.join(inp_algo).saveAsTextFile(output_path)
+    # print('batch,completed')
     sc.stop()
