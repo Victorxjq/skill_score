eb31d271ca838e8e1cfef20d795d003e8a1f04cb
奚骏泉 <junquan.xi@ifchange.com>
2019-02-21 16:33:43 +0800

    update run.py

--- skill_score/process_result.py
+++ skill_score/process_result.py
@@ -4,6 +4,7 @@ import json
 model = gensim.models.KeyedVectors.load_word2vec_format('./embedding/agg_processed_feature_skill_func4_20190125.txt',binary = False,encoding='utf-8')
 if __name__ == '__main__':
     output_file=open('./embedding/output.txt','w')
+    index=0
     with open('./embedding/skill_level_score.jsonl','r') as input_file:
         for line in input_file.readlines():
             line=line.strip('\n')
@@ -18,8 +19,10 @@ if __name__ == '__main__':
                 except:
                     skill_sim=0
                 if skill_sim>0:
-                    skill_name_list.append(skill_name+'$$'+skill_score)
+                    skill_name_list.append(skill_name+'$$'+str(skill_score))
                 # print('func4_name/skill_name/similarity/score',func4_name,skill_name,skill_sim,skill_score)
+            print(index)
+            index+=1
             output_file.write(func4_name+'\t'+'\t'.join(skill_name_list))
             output_file.write('\n')
     output_file.close()
