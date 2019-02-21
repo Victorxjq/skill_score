4ce57cf9e64cc6abe9edede249326e5d1824b0b9
奚骏泉 <junquan.xi@ifchange.com>
2019-02-21 16:31:26 +0800

    update run.py

--- skill_score/process_result.py
+++ skill_score/process_result.py
@@ -3,11 +3,13 @@ import json
 # 简历解析
 model = gensim.models.KeyedVectors.load_word2vec_format('./embedding/agg_processed_feature_skill_func4_20190125.txt',binary = False,encoding='utf-8')
 if __name__ == '__main__':
+    output_file=open('./embedding/output.txt','w')
     with open('./embedding/skill_level_score.jsonl','r') as input_file:
         for line in input_file.readlines():
             line=line.strip('\n')
             line=json.loads(line)
             func4_name='#funlvl4$'+line['function_name_lvl4'].replace(' ','_').lower()
+            skill_name_list=[]
             for skill in list(line['skill_level'].keys()):
                 skill_name='#skill$'+skill.replace(' ','_').lower()
                 skill_score=line['skill_level'][skill][0]
@@ -15,5 +17,10 @@ if __name__ == '__main__':
                     skill_sim=model.similarity(func4_name,skill_name)
                 except:
                     skill_sim=0
-                print('func4_name/skill_name/similarity/score',func4_name,skill_name,skill_sim,skill_score)
+                if skill_sim>0:
+                    skill_name_list.append(skill_name+'$$'+skill_score)
+                # print('func4_name/skill_name/similarity/score',func4_name,skill_name,skill_sim,skill_score)
+            output_file.write(func4_name+'\t'+'\t'.join(skill_name_list))
+            output_file.write('\n')
+    output_file.close()
 
